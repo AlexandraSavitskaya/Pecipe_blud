a37b6edc3921a8e7e4104ca7caf81aa37884d6ad
AlexandraSavitskaya <150243337+AlexandraSavitskaya@users.noreply.github.com>
2025-06-16 00:44:16 +0300

    С базой данных работает, регистрация работает, кнопки внутри работают. 0.42 16.062025

--- app/src/main/java/com/example/pecipe_blud/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pecipe_blud/ui/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.pecipe_blud.databinding.FragmentHomeBinding
+import com.example.pecipe_blud.databinding.FragmentProfilBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProfilBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
